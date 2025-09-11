7edd4a7bcff59ea272376ec88284ae6b5a407177
Griffin <gaheyrich@gmail.com>
2025-09-11 17:16:19 +0000

    add libpmemobj bindings for rust

--- build.rs
+++ build.rs
@@ -4,23 +4,26 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    println!("cargo:rerun-if-changed=wrapper.h");
-    
-    println!("cargo:rustc-link-lib=numa");
+    println!("cargo:rerun-if-changed=wrapper.h");    
+    //println!("cargo:rustc-link-lib=numa");
+
+    println!("cargo:rustc-link-lib=pmemobj");
 
     // Generate bindings
     let bindings = bindgen::Builder::default()
-        .header("/home/griffin/code/cxl/paper-cache-cxl/wrapper.h")
+        .header("/home/griffin/code/cxl_paper_cache/paper-cache-cxl/wrapper.h")
         .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
         .generate()
         .expect("Unable to generate bindings");
 
+    
+    println!("Generated bindings");
     // Write the bindings to the $OUT_DIR/bindings.rs
     //let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    
-    let out_path = PathBuf::from("/home/griffin/code/cxl/paper-cache-cxl");
+
+    let out_path = PathBuf::from("/home/griffin/code/cxl_paper_cache/paper-cache-cxl/");
     bindings
-        .write_to_file(out_path.join("bindings.rs"))
+        .write_to_file(out_path.join("pmemobj_bindings.rs"))
         .expect("Couldn't write bindings!");
 
     println!("DONE");
--- wrapper.h
+++ wrapper.h
@@ -1,2 +1,1 @@
-#include <numa.h>
-#include <numaif.h>
+#include<libpmemobj.h>
