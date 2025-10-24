2f55e663c5cb08116d143434359386161b7f8ece
Griffin Andrew <gaheyrich@gmail.com>
2025-10-24 18:39:38 +0000

    compile err

--- src/lib.rs
+++ src/lib.rs
@@ -12,11 +12,9 @@ pub mod allocator;
 #[cfg(feature = "allocator_api")]
 use crate::allocator::HybridObjects as Hybrid;
 
-#[cfg(feature = "allocator_api")]
-println!("Compiling with allocator_api feature enabled.");
 
-//#[cfg(not(feature = "allocator_api"))]
-//compile_error!("The 'allocator_api' feature must be enabled to use the allocator.");
+#[cfg(not(feature = "allocator_api"))]
+compile_error!("The 'allocator_api' feature must be enabled to use the allocator.");
 
 mod error;
 mod worker;
