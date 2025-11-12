d320fdfe8c2a88bef1f9acbb24f79f2a472b6550
Griffin Andrew <gaheyrich@gmail.com>
2025-11-12 22:34:14 +0000

    not using allocator api atm

--- src/lib.rs
+++ src/lib.rs
@@ -503,6 +503,7 @@ where
 		let hashed_key = self.hash_key(&key);
 
 		//println!("set for key {:?}: {:?}", key, std::mem::size_of_val(&value));
+		println!("set pointers: key: {:p}, value: {:p}", &key, &value);
 
 		let object = Object::new(key, value, ttl);
 		//let base_size = self.overhead_manager.base_size(&object);
