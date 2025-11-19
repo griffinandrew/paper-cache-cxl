f9fb5cdc7efd6d050db039200479b768ea3a06aa
Griffin Andrew <gaheyrich@gmail.com>
2025-11-19 17:00:14 +0000

    correclty allocating values in pmem

--- src/allocator.rs
+++ src/allocator.rs
@@ -45,12 +45,12 @@ unsafe impl GlobalAlloc for HybridObjects {
                         dax_size,
                         );
                     #[cfg(debug_assertions)] {println!("Initialized PMEM allocator with DAX path /dev/dax0.0 and size {}", dax_size);}
-                    println!("CACHE: Initialized PMEM allocator with DAX path /dev/dax0.0 and size {}", dax_size);
+                    //println!("CACHE: Initialized PMEM allocator with DAX path /dev/dax0.0 and size {}", dax_size);
                 });
             }
             let ptr = allocator_bindings::umf_alloc(layout.size(), layout.align()) as *mut u8;
             if ptr.is_null() { println!("Failed to allocate PMEM in cache allocator"); return ptr::null_mut(); }
-            println!("CACHE: Allocated {} bytes from PMEM", layout.size());
+            //println!("CACHE: Allocated {} bytes from PMEM", layout.size());
             //println!("CACHE: allocated size (align): {}", layout.align());
             //println!("CACHE: allocated ptr: {:p}", ptr);
             return ptr;
