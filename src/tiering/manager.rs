4a34c1df804c07fef15f823307ba16c360f20e8f
Griffin Andrew <gaheyrich@gmail.com>
2025-12-08 17:18:34 +0000

    it was calcuing size.. not using one I had specified

--- src/tiering/manager.rs
+++ src/tiering/manager.rs
@@ -40,7 +40,7 @@ pub struct TieringConfig {
 impl Default for TieringConfig {
     fn default() -> Self {
         TieringConfig {
-            dram_threshold: 2 * 1024 * 1024 , // 2 MB default
+            dram_threshold: 100 * 1024 * 1024 , // 100 MB default
             high_water_mark: 0.9,
             low_water_mark: 0.7,
             hotness_threshold: 2, // Promote after 2 accesses
