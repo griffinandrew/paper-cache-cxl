c6cee07b29b3e4bce2d0388bd504f9f8070b98ce
Griffin Andrew <gaheyrich@gmail.com>
2025-12-09 02:54:54 +0000

    diff mb size for dram

--- src/tiering/manager.rs
+++ src/tiering/manager.rs
@@ -40,7 +40,7 @@ pub struct TieringConfig {
 impl Default for TieringConfig {
     fn default() -> Self {
         TieringConfig {
-            dram_threshold: 750 * 1024 * 1024, // 750 MB default
+            dram_threshold: 1024 * 1024 * 1024, // 750 MB default
             high_water_mark: 0.95,
             low_water_mark: 0.7,
             hotness_threshold: 3, // Promote after 3 accesses
