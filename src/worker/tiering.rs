297825dc692f4937e007ca6419c2a830eb7f36f4
Griffin Andrew <gaheyrich@gmail.com>
2025-12-09 02:38:03 +0000

    diff mb size for dram

--- src/worker/tiering.rs
+++ src/worker/tiering.rs
@@ -40,7 +40,7 @@ use crate::{
 };
 
 /// Interval for periodic tiering decisions (migration checks)
-const TIERING_INTERVAL: Duration = Duration::from_secs(1);
+const TIERING_INTERVAL: Duration = Duration::from_secs(10);
 
 pub struct TieringWorker<K, V> {
     listener: Receiver<WorkerEvent>,
