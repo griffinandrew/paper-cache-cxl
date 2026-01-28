3a073d70d0379866369923a0250e801f3afdb098
Griffin Andrew <gaheyrich@gmail.com>
2026-01-28 20:12:17 +0000

    add back value copy to flatmap pmem

--- src/lib.rs
+++ src/lib.rs
@@ -3499,20 +3499,20 @@ where
 		let hashed_key = self.hash_key(key);
 
 		let result = match self.objects.read().unwrap().get(&hashed_key) {
-			Some(object) if object.key_matches(key) && !object.is_expired() => {
-			//Some(object) => {
-				self.status.incr_hits();
+			//Some(object) if object.key_matches(key) && !object.is_expired() => {
+			Some(object) => {
+				//self.status.incr_hits();
 				let arc_val = object.data();
 				Ok(arc_val.as_ref().to_vec())
 				//Ok(vec![])
 			},
 			_ => {
-				self.status.incr_misses();
+				//self.status.incr_misses();
 				Err(CacheError::KeyNotFound)
 			},
 		};
 
-		self.broadcast(WorkerEvent::Get(hashed_key, result.is_ok()))?;
+		//self.broadcast(WorkerEvent::Get(hashed_key, result.is_ok()))?;
 		result
 	}
 
@@ -3774,21 +3774,21 @@ where
 		let hashed_key = self.hash_key(key);
 
 		let result = match self.objects.read().unwrap().get(&hashed_key) {
-			//Some(object) if object.key_matches(key) && !object.is_expired() => {
-			Some(object) => {
-				//self.status.incr_hits();
-				//let arc_val = object.data();
-				//Ok(arc_val.as_ref().to_vec())
-				Ok(vec![])
+			Some(object) if object.key_matches(key) && !object.is_expired() => {
+			//Some(object) => {
+				self.status.incr_hits();
+				let arc_val = object.data();
+				Ok(arc_val.as_ref().to_vec())
+				//Ok(vec![])
 			},
 			_ => {
-				//self.status.incr_misses();
+				self.status.incr_misses();
 				Err(CacheError::KeyNotFound)
 			},
 		};
 
 		
-		//self.broadcast(WorkerEvent::Get(hashed_key, result.is_ok()))?;
+		self.broadcast(WorkerEvent::Get(hashed_key, result.is_ok()))?;
 
 		result
 	}
