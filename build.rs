8e95043c124cf2ee98d3a60d55825ed7ae586506
Griffin Andrew <gaheyrich@gmail.com>
2026-03-04 21:42:05 +0000

    add new biild and branch for vm ....

--- build.rs
+++ build.rs
@@ -3,53 +3,32 @@ extern crate bindgen;
 use std::path::{Path, PathBuf};
 
 fn main() {
-    let wrapper_path = "/home/griffin/cxl_baseline/paper-server-cxl/wrapper.h";
-    
-    // Only run bindgen if the wrapper exists
+    let wrapper_path = "/home/griff/work/wrapper.h";
+
+    let umf_lib_dir = "/home/griff/umf-install/lib64";
+    let umf_include_dir = "/home/griff/umf-install/include";
+
     if Path::new(wrapper_path).exists() {
         println!("cargo:rerun-if-changed=wrapper.h");
-        //these work for host but not for vm so modifying.... temp....
-        //println!("cargo:rustc-link-lib=umf");
-        //println!("cargo:rustc-link-search=native=/home/griffin/libs/unified-memory-framework/lib");
-        //println!("cargo:rustc-link-lib=umf_allocator");
 
-        println!("cargo:rustc-link-search=native={}", std::env::var("HOME").unwrap() + "/umf-install/lib64");
-        println!("cargo:rustc-link-lib=dylib=umf");
-        println!("cargo:rustc-link-lib=dylib=umf_allocator");
+        // Tell Rust where UMF lives
+        println!("cargo:rustc-link-search=native={}", umf_lib_dir);
+        println!("cargo:rustc-link-lib=dylib=umf"); // use shared library
 
-        /*
-        // Generate bindings
-        let bindings = bindgen::Builder::default()
-            .header(wrapper_path) // Ensure this header includes memkind headers
-            .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
-            .generate()
-            .expect("Unable to generate bindings");
-
-        println!("Generated bindings");
-
-        let out_path = PathBuf::from("/home/griffin/cxl_baseline/paper-server-cxl/src/");
-        bindings
-            .write_to_file(out_path.join("umf_bindings.rs"))
-            .expect("Couldn't write bindings!");
-
-        println!("DONE");
-        */
-
+        // Compile your wrapper C code
         cc::Build::new()
             .file("umf_allocator/umf_allocator_wrapper.c")
             .include("umf_allocator")
-            .compile("umf_allocator"); 
+            .include(umf_include_dir)
+            .compile("umf_allocator_wrapper");
 
-        println!("Compiled umf_allocator.c");
     } else {
         println!("cargo:warning=UMF wrapper.h not found; skipping bindgen");
-        // Create a minimal stub if umf_bindings.rs doesn't exist
+
         let umf_bindings_path = PathBuf::from("src/umf_bindings.rs");
         if !umf_bindings_path.exists() {
             std::fs::write(&umf_bindings_path, "// Stub UMF bindings\n")
                 .expect("Could not write stub umf_bindings.rs");
         }
-        
     }
-    
 }
