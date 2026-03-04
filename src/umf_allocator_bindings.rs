4eb6c0a2f2e58fdd75d13322a55ec0ae89c8b82e
Griffin Andrew <gaheyrich@gmail.com>
2026-03-04 21:48:37 +0000

    update bindings for numa

--- src/umf_allocator_bindings.rs
+++ src/umf_allocator_bindings.rs
@@ -3,9 +3,12 @@
 //solo numa and daxdev config
 
 unsafe extern "C" {
-    pub fn umf_allocator_init(dax_path: *const libc::c_char, dax_size: usize) -> libc::c_int;
 
-    //pub fn umf_allocator_init(numa_node: libc::c_int) -> libc::c_int;
+    //this one is for devdax 
+    //pub fn umf_allocator_init(dax_path: *const libc::c_char, dax_size: usize) -> libc::c_int;
+
+    //this one is for numa... 
+    pub fn umf_allocator_init(numa_node: libc::c_int) -> libc::c_int;
     pub fn umf_alloc(size: usize, align: usize) -> *mut libc::c_void;
     pub fn umf_dealloc(ptr: *mut libc::c_void);
     pub fn umf_allocator_finalize();
