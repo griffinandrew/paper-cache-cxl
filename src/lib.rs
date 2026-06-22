21ecea8f2df59ea1120b47cc3eeb47178fea8312
Griffin Andrew <gaheyrich@gmail.com>
2026-06-22 14:09:28 +0000

    set broadcast

--- src/lib.rs
+++ src/lib.rs
@@ -1246,27 +1246,27 @@ where
 
 	pub fn set(&self, key: K, value: &[u8], ttl: Option<u32>) -> Result<(), CacheError> {
 		
-		//let t0 = rdtsc();
+		let t0 = rdtsc();
 		let hashed_key = self.hash_key(&key);
-		//let t1 = rdtsc();
-		//PHASE_PRE_ALLOC.record(t1 - t0);
+		let t1 = rdtsc();
+		PHASE_PRE_ALLOC.record(t1 - t0);
 
 		//allocate it as a regular buffer... 
 		//let val_buf: Box<[u8]> = value.to_vec().into_boxed_slice();
 
 		//let key_buff = Box::new(key);
 
-		//let t2_start = rdtsc();
-		//let mut val_buf: Vec<u8> = Vec::with_capacity(value.len());
+		let t2_start = rdtsc();
+		let mut val_buf: Vec<u8> = Vec::with_capacity(value.len());
 
-		//let t2_end = rdtsc();
-		//PHASE_ALLOC.record(t2_end - t2_start);
+		let t2_end = rdtsc();
+		PHASE_ALLOC.record(t2_end - t2_start);
 
 		// === phase 3: memcpy value into PMEM ===
-		//let t3_start = rdtsc();
-		//val_buf.extend_from_slice(value);
-		//let val_buf: BufferDRAM = val_buf.into_boxed_slice();
-		let val_buf: BufferDRAM = Box::clone_from_ref(value);
+		let t3_start = rdtsc();
+		val_buf.extend_from_slice(value);
+		let val_buf: BufferDRAM = val_buf.into_boxed_slice();
+		//let val_buf: BufferDRAM = Box::clone_from_ref(value);
 
 		/* 
 		unsafe {
@@ -1289,13 +1289,13 @@ where
 			}
 		}
 		*/
-		//let t3_end = rdtsc();
-		//PHASE_MEMCPY.record(t3_end - t3_start);
+		let t3_end = rdtsc();
+		PHASE_MEMCPY.record(t3_end - t3_start);
 
-		//let t4_start = rdtsc();
+		let t4_start = rdtsc();
 		let object = Object::new(key, val_buf, ttl);
-		//let t4_end = rdtsc();
-		//PHASE_POST.record(t4_end - t4_start);
+		let t4_end = rdtsc();
+		PHASE_POST.record(t4_end - t4_start);
 		let base_size = self.overhead_manager.base_size(&object);
 		let expiry = object.expiry();
 
@@ -1309,7 +1309,7 @@ where
 
 		self.status.incr_sets();
 
-		//let t5_start = rdtsc();
+		let t5_start = rdtsc();
 		let old_object_info = self.objects
 			.insert(hashed_key, object)
 			.map(|old_object| {
@@ -1318,8 +1318,8 @@ where
 
 				(base_size, expiry)
 			});
-		//let t5_end = rdtsc();
-		//PHASE_INSERT.record(t5_end - t5_start);
+		let t5_end = rdtsc();
+		PHASE_INSERT.record(t5_end - t5_start);
 
 		let base_size_delta = if let Some((old_object_size, _)) = old_object_info {
 			base_size as i64 - old_object_size as i64
@@ -1330,10 +1330,10 @@ where
 		};
 
 		self.status.update_base_used_size(base_size_delta);
-		//let t6_start = rdtsc();
+		let t6_start = rdtsc();
 		self.broadcast(WorkerEvent::Set(hashed_key, base_size, expiry, old_object_info))?;
-		//let t6_end = rdtsc();
-		//PHASE_SET_BROADCAST.record(t6_end - t6_start);
+		let t6_end = rdtsc();
+		PHASE_SET_BROADCAST.record(t6_end - t6_start);
 
 		Ok(())
 	}
@@ -2131,10 +2131,10 @@ where
 		K: 'static + Eq + Hash + TypeSize + std::fmt::Debug,
 	{
 
-		//let t0 = rdtsc();
+		let t0 = rdtsc();
 		let hashed_key = self.hash_key(&key);
-		//let t1 = rdtsc();
-		//PHASE_PRE_ALLOC.record(t1 - t0);
+		let t1 = rdtsc();
+		PHASE_PRE_ALLOC.record(t1 - t0);
 
 
 		//println!("CACHE: set called for key {:?} with value size {}", key, value.len());
@@ -2201,17 +2201,17 @@ where
 			//pub fn clone_from_ref_in(src: &T, alloc: A) -> Box<T, A>
 
 			// === phase 2: allocation of value buffer ===
-			//let t2_start = rdtsc();
-			//let mut val_buf: Vec<u8, Hybrid> = Vec::with_capacity_in(value.len(), Hybrid);
-			let val_buf: BufferPMEM = Box::clone_from_ref_in(value, Hybrid);
+			let t2_start = rdtsc();
+			let mut val_buf: Vec<u8, Hybrid> = Vec::with_capacity_in(value.len(), Hybrid);
+			//let val_buf: BufferPMEM = Box::clone_from_ref_in(value, Hybrid);
 			//let mut uninit_buf = Box::new_uninit_slice_in(value.len(), Hybrid);
-			//let t2_end = rdtsc();
-			//PHASE_ALLOC.record(t2_end - t2_start);
+			let t2_end = rdtsc();
+			PHASE_ALLOC.record(t2_end - t2_start);
 
 			// === phase 3: memcpy value into PMEM ===
-			//let t3_start = rdtsc();
-			//val_buf.extend_from_slice(value);
-			//let val_buf: BufferPMEM = val_buf.into_boxed_slice();
+			let t3_start = rdtsc();
+			val_buf.extend_from_slice(value);
+			let val_buf: BufferPMEM = val_buf.into_boxed_slice();
 
 			/*
 			unsafe {
@@ -2249,8 +2249,8 @@ where
 			//let val_buf: BufferPMEM = unsafe { uninit_buf.assume_init() };
 
 			//let val_buf: BufferPMEM = { uninit_buf.assume_init() };
-			//let t3_end = rdtsc();
-			//PHASE_MEMCPY.record(t3_end - t3_start);
+			let t3_end = rdtsc();
+			PHASE_MEMCPY.record(t3_end - t3_start);
 
 			//let key_buf: BufferPMEM = 
 
@@ -2261,10 +2261,10 @@ where
 			//let key_buf: BufferPMEM = key.to_vec_in(Hybrid).into_boxed_slice();
 
 			//the key should also be in pmem... this is stale or wrong... mut have changed it back??
-			//let t4_start = rdtsc();
+			let t4_start = rdtsc();
 			let object = Object::new(key, val_buf, ttl);
-			//let t4_end = rdtsc();
-			//PHASE_POST.record(t4_end - t4_start);
+			let t4_end = rdtsc();
+			PHASE_POST.record(t4_end - t4_start);
 
 			let base_size = self.overhead_manager.base_size(&object);
 			let expiry = object.expiry();
@@ -2279,7 +2279,7 @@ where
 
 			self.status.incr_sets();
 
-			//let t5_start = rdtsc();
+			let t5_start = rdtsc();
 			let old_object_info = self.objects
 				.insert(hashed_key, object)
 				.map(|old_object| {
@@ -2288,8 +2288,8 @@ where
 
 					(base_size, expiry)
 				});
-			//let t5_end = rdtsc();
-			//PHASE_INSERT.record(t5_end - t5_start);
+			let t5_end = rdtsc();
+			PHASE_INSERT.record(t5_end - t5_start);
 
 			let base_size_delta = if let Some((old_object_size, _)) = old_object_info {
 				base_size as i64 - old_object_size as i64
@@ -2300,10 +2300,10 @@ where
 			};
 
 			self.status.update_base_used_size(base_size_delta);
-			//let t6_start = rdtsc();
+			let t6_start = rdtsc();
 			self.broadcast(WorkerEvent::Set(hashed_key, base_size, expiry, old_object_info))?;
-			//let t6_end = rdtsc();
-			//PHASE_SET_BROADCAST.record(t6_end - t6_start);
+			let t6_end = rdtsc();
+			PHASE_SET_BROADCAST.record(t6_end - t6_start);
 			Ok(())
 		}
 
